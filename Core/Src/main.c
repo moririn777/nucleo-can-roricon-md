de198df3d4af84b38e58f3f5d1abf582e2dafa3f
moririn777 <moriya8229@gmail.com>
2024-08-05 21:20:18 +0900

    feat: CANでMDを動かす

---
+++ Core/Src/main.c
@@ -0,0 +1,373 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+CAN_HandleTypeDef hcan;
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+CAN_FilterTypeDef filter;
+CAN_RxHeaderTypeDef RxHeader;
+UART_HandleTypeDef huart2;
+
+uint8_t RxData[4];
+uint32_t id, dlc, data;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
+
+void Filter_Init();
+
+int main() {
+	setbuf(stdout, NULL);
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
+	MX_CAN_Init();
+	MX_TIM1_Init();
+	MX_TIM2_Init();
+	MX_USART2_UART_Init();
+	MX_TIM3_Init();
+
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_2);
+
+	Filter_Init();
+	printf("Start CAN Rx\r\n");
+	HAL_CAN_Start(&hcan);
+
+	
+	if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING)) {
+		printf("Error: Can't activate notification.");
+		Error_Handler();
+	}
+
+	while (true) {
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,1);
+		__HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_1,RxData[2]);
+		__HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_2,RxData[3]);
+	}
+}
+
+int _write(int file, char *ptr, int len) {
+	HAL_UART_Transmit(&huart2, (uint8_t*) ptr, len, 10);
+	return len;
+}
+
+void Filter_Init() {
+	filter.FilterIdHigh = 0;  			//canIDの上位16bit
+	filter.FilterIdLow = 0;  			//canIDの下位16bit(標準IDは11bitのため０)
+	filter.FilterMaskIdHigh = 0;  		//上位16bitの中から比較する範囲を指定
+	filter.FilterMaskIdLow = 0;  		//下位16bitのを比較する範囲指定(標準IDは11bitのため０)
+	filter.FilterScale = CAN_FILTERSCALE_32BIT;  			//filterスケール(32bit)
+	filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;       	//受信メッセージを格納する場所を指定
+	filter.FilterBank = 0;                      			//filterを複数利用する時に使用
+	filter.FilterMode = CAN_FILTERMODE_IDMASK; 				//filterModeの指定(IDMASK or IDLIST)
+	filter.SlaveStartFilterBank = 14;
+	filter.FilterActivation = ENABLE;
+	HAL_CAN_ConfigFilter(&hcan, &filter);
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+	printf("Run: Interrupt.");
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK) {
+		id = (RxHeader.IDE == CAN_ID_STD) ? RxHeader.StdId : RxHeader.ExtId; 		//IDEがSTDならばStdIdを取得
+		dlc = RxHeader.DLC; 		//フレームの大きさ
+
+		data = 0x00;
+		for (uint8_t i = 0; i < 4; i++) {
+			data |= (uint32_t) RxData[i] << (24 - 8 * i);
+		}
+
+		printf("ID: %#x\r\n", (int) id);
+		printf("DLC: %#x\r\n", (int) dlc);
+
+		for (uint8_t i = 0; i < 4; i++) {
+			printf("RxData[%d]: %#x ", i, RxData[i]);
+		}
+		printf("\r\n");
+
+		printf("data: %u\r\n", data);
+
+	}
+}
+
+/*AutoGenerate*/
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_TIM1;
+	PeriphClkInit.Tim1ClockSelection = RCC_TIM1CLK_HCLK;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+
+static void MX_CAN_Init(void) {
+
+	hcan.Instance = CAN;
+	hcan.Init.Prescaler = 4;
+	hcan.Init.Mode = CAN_MODE_NORMAL;
+	hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+	hcan.Init.TimeSeg1 = CAN_BS1_11TQ;
+	hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
+	hcan.Init.TimeTriggeredMode = DISABLE;
+	hcan.Init.AutoBusOff = DISABLE;
+	hcan.Init.AutoWakeUp = DISABLE;
+	hcan.Init.AutoRetransmission = DISABLE;
+	hcan.Init.ReceiveFifoLocked = DISABLE;
+	hcan.Init.TransmitFifoPriority = DISABLE;
+	if (HAL_CAN_Init(&hcan) != HAL_OK) {
+		Error_Handler();
+	}
+}
+	static void MX_TIM1_Init(void) {
+
+		TIM_Encoder_InitTypeDef sConfig = { 0 };
+		TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+
+		htim1.Instance = TIM1;
+		htim1.Init.Prescaler = 0;
+		htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim1.Init.Period = 65535;
+		htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim1.Init.RepetitionCounter = 0;
+		htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
+		sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC1Filter = 0;
+		sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC2Filter = 0;
+		if (HAL_TIM_Encoder_Init(&htim1, &sConfig) != HAL_OK) {
+			Error_Handler();
+		}
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+
+	}
+
+	static void MX_TIM2_Init(void) {
+
+		TIM_Encoder_InitTypeDef sConfig = { 0 };
+		TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+
+		/* USER CODE BEGIN TIM2_Init 1 */
+
+		/* USER CODE END TIM2_Init 1 */
+		htim2.Instance = TIM2;
+		htim2.Init.Prescaler = 0;
+		htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim2.Init.Period = 65535;
+		htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
+		sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC1Filter = 0;
+		sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC2Filter = 0;
+		if (HAL_TIM_Encoder_Init(&htim2, &sConfig) != HAL_OK) {
+			Error_Handler();
+		}
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		/* USER CODE BEGIN TIM2_Init 2 */
+
+		/* USER CODE END TIM2_Init 2 */
+
+	}
+
+	/**
+	 * @brief TIM3 Initialization Function
+	 * @param None
+	 * @retval None
+	 */
+	static void MX_TIM3_Init(void) {
+
+		/* USER CODE BEGIN TIM3_Init 0 */
+
+		/* USER CODE END TIM3_Init 0 */
+
+		TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+		TIM_OC_InitTypeDef sConfigOC = { 0 };
+
+		/* USER CODE BEGIN TIM3_Init 1 */
+
+		/* USER CODE END TIM3_Init 1 */
+		htim3.Instance = TIM3;
+		htim3.Init.Prescaler = 24 - 1;
+		htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim3.Init.Period = 1000 - 1;
+		htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		if (HAL_TIM_PWM_Init(&htim3) != HAL_OK) {
+			Error_Handler();
+		}
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		sConfigOC.OCMode = TIM_OCMODE_PWM1;
+		sConfigOC.Pulse = 0;
+		sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+		sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+		if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		/* USER CODE BEGIN TIM3_Init 2 */
+
+		/* USER CODE END TIM3_Init 2 */
+		HAL_TIM_MspPostInit(&htim3);
+
+	}
+
+
+	static void MX_USART2_UART_Init(void) {
+		huart2.Instance = USART2;
+		huart2.Init.BaudRate = 115200;
+		huart2.Init.WordLength = UART_WORDLENGTH_8B;
+		huart2.Init.StopBits = UART_STOPBITS_1;
+		huart2.Init.Parity = UART_PARITY_NONE;
+		huart2.Init.Mode = UART_MODE_TX_RX;
+		huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+		huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+		huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+		huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+		if (HAL_UART_Init(&huart2) != HAL_OK) {
+			Error_Handler();
+		}
+	}
+
+	static void MX_GPIO_Init(void) {
+		GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+		/* USER CODE BEGIN MX_GPIO_Init_1 */
+		/* USER CODE END MX_GPIO_Init_1 */
+
+		/* GPIO Ports Clock Enable */
+		__HAL_RCC_GPIOF_CLK_ENABLE();
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+
+		/*Configure GPIO pin Output Level */
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3 | GPIO_PIN_5, GPIO_PIN_RESET);
+
+		/*Configure GPIO pin Output Level */
+		HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+		/*Configure GPIO pins : PA3 PA5 */
+		GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_5;
+		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		/*Configure GPIO pins : PB0 PB1 */
+		GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+		GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+		/*Configure GPIO pin : LED_Pin */
+		GPIO_InitStruct.Pin = LED_Pin;
+		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+		/* EXTI interrupt init*/
+		HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+		HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
+		/* USER CODE BEGIN MX_GPIO_Init_2 */
+		/* USER CODE END MX_GPIO_Init_2 */
+	}
+
+	/* USER CODE BEGIN 4 */
+
+	/* USER CODE END 4 */
+
+	/**
+	 * @brief  This function is executed in case of error occurrence.
+	 * @retval None
+	 */
+	void Error_Handler(void) {
+		/* USER CODE BEGIN Error_Handler_Debug */
+		/* User can add his own implementation to report the HAL error return state */
+		__disable_irq();
+		while (1) {
+		}
+		/* USER CODE END Error_Handler_Debug */
+	}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
